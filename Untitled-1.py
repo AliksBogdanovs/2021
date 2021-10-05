4b7c65a611029b04bab0bc778477b7c32b02214f
AliksBogdanovs <90691932+AliksBogdanovs@users.noreply.github.com>
2021-10-05 13:52:05 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+# Problēma: Vecvecākiem nepieciešams aprēķināt ievārījuma izmaksas
+# 1. specifikācija
+# Uzrakstīt funkciju, kas aprēķina ābolu ievārijuma izmaksas
+#ievaddati: cukura cena kilogramā, cukura ābolu garšvielu daudzums, ābolu daudzums kg
+
+recepte = {"cukurs":0.6, "kanelis":0.008,"āboli":2.0,"ūdeni":0.2}
+cena = {"cukurs": 0.75, "kanelis":0.3,"āboli":0.0, "ūdeni":0.0}
+
+cukurs= 0.75
+kanelis= 0.3
+āboli= 0.0
+ūdeni= 0.0
+
+num = recepte["cukurs"]
+num2 = recepte["kanelis"]
+sum = cukurs+kanelis
+while (num != 0):
+
+print("recepte: ", sum)
